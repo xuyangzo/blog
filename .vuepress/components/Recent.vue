a0e5705225c5558728bb53f263498fb126eda495
xuyangzo <xuyangzo@usc.edu>
2019-10-29 21:51:54 -0700

    recent page animation fix

--- .vuepress/components/Recent.vue
+++ .vuepress/components/Recent.vue
@@ -5,15 +5,15 @@
         dense
         class="article-block mt-2 mb-2 pt-2"
         v-for="(article, index) in articles"
-        :key="articles.title"
+        :key="article.title"
         v-observe-visibility="{
-          callback: (isVisible) => { visibilityChanged(isVisible, index) },
+          callback: (isVisible) => { visibilityChanged(isVisible, article) },
           intersection: {
             threshold: 0.2
           },
           once: true
         }"
-        :class="{ 'article-block-appear': index <= visibleIndex }"
+        :class="{ 'article-block-appear': article.isVisible }"
       >
         <v-col cols="12">
           <v-card color="#fff" light class="custom-card" @click="goToArticle(article.path)">
@@ -105,8 +105,8 @@ export default {
       }
       return null;
     },
-    visibilityChanged(isVisible, index) {
-      if (isVisible) this.visibleIndex = index;
+    visibilityChanged(isVisible, article) {
+      if (isVisible) article.isVisible = true;
     }
   },
   mounted() {
@@ -127,7 +127,12 @@ export default {
       return parseInt(date2.slice(3, 5)) - parseInt(date1.slice(3, 5));
     });
 
-    this.articles = articles.filter(page => page.frontmatter.tags);
+    this.articles = articles
+      .filter(page => page.frontmatter.tags)
+      .map(article => {
+        article.isVisible = false;
+        return article;
+      });
 
     // 获取当前页数
     this.page = parseInt(this.$route.hash.slice(1)) || 1;
@@ -153,13 +158,17 @@ export default {
 </script>
 
 <style lang="stylus">
-@keyframes scale {
+@keyframes myFadeInUp {
   0% {
-    transform: scale(0);
+    transform: translate(0, 200px);
+    -webkit-transform: translate(0, 200px);
+    opacity: 0;
   }
 
   100% {
-    transform: scale(1);
+    transform: translate(0, 0);
+    -webkit-transform: translate(0, 0);
+    opacity: 1;
   }
 }
 
@@ -167,7 +176,7 @@ export default {
   .custom-card {
     box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
     text-align: left !important;
-    transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+    transition: transform 1s cubic-bezier(0.075, 0.82, 0.165, 1);
 
     &:hover {
       color: mediumpurple;
@@ -177,17 +186,19 @@ export default {
   }
 
   .article-block {
-    transform: scale(0);
+    opacity: 0;
   }
 
   .article-block-appear {
-    animation: scale 1s forwards cubic-bezier(0.075, 0.82, 0.165, 1);
+    animation: myFadeInUp 2s forwards cubic-bezier(0.075, 0.82, 0.165, 1);
   }
 }
 
 @media (max-width: 400px) {
   .recent {
     .custom-card {
+      transition: none;
+
       &:hover {
         transform: none;
         -webkit-transform: none;
